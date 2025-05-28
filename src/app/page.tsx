affb88fe415e7dada32fea23d8448db8f9fa8bc0
ALOK0078 <alokkumar0709003@gmail.com>
2025-05-29 00:05:56 +0530

    03 added auth interface

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,45 @@
-import Image from "next/image";
-
+"use client";
+import { authClient } from "@/lib/auth-client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {useState} from "react";
 export default function Home() {
+  const{data:session} = authClient.useSession();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const onSubmit =()=>{
+    authClient.signUp.email({
+      name,
+      email,
+      password
+    },{
+      onError:()=>{
+        window.alert("something went wrong");
+      },
+      onSuccess:()=>{
+        window.alert("success");
+      }
+    })
+  }
+  if(session){
+    return (
+      <div className="flex flex-col p-4 gap-y-4">
+       <p>Logged in as {session.user.name}</p>
+       <Button onClick={() => authClient.signOut()}>
+         Sign Out
+       </Button>
+      </div>
+    );
+  }
   return (
-    <div className="text-4xl font-bold text-green-400">
-      Hello world
+    <div className = "p-4 flex flex-col gap-y-4">
+      <Input placeholder="name" value ={name} onChange ={(e)=>setName(e.target.value)}/>
+      <Input placeholder="email" value ={email} onChange ={(e)=>setEmail(e.target.value)}/>
+      <Input placeholder="password" value ={password} onChange ={(e)=>setPassword(e.target.value)}/>
+      <Button onClick={onSubmit}>
+        Create User
+      </Button>
     </div>
   );
 }
