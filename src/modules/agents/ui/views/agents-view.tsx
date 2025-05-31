0ef46266b9ac6e96e8d7af5a03c21e68f9f25876
ALOK0078 <alokkumar0709003@gmail.com>
2025-05-31 22:32:25 +0530

    12 agent-view

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -11,9 +11,11 @@ import { columns} from "../components/columns";
 import { EmptyState } from "@/components/empty-state";
 import { useAgentsFilters } from "../../hooks/use-agents-filters";
 import { DataPagination } from "../components/data-pagination";
+import { useRouter } from "next/navigation";
 
 
 export const AgentsView =()=>{
+    const router = useRouter();
     const [filters,setFilters] = useAgentsFilters();
     const trpc = useTRPC();
     const {data } = useSuspenseQuery(trpc.agents.getMany.queryOptions({
@@ -22,7 +24,7 @@ export const AgentsView =()=>{
     
     return(
         <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-        <DataTable data={data.items} columns={columns}/>
+        <DataTable data={data.items} columns={columns} onRowClick={(row)=>router.push(`/agents/${row.id}`)}/>
         <DataPagination
         page={filters.page}
         totalPages={data.totalPages}
@@ -42,6 +44,7 @@ export const AgentsViewLoading =()=>{
         <LoadingState
         title ="Loading Agents" description ="This may take a few seconds"
         />
+        
     )
 }
 export const AgentsViewError =()=>{
