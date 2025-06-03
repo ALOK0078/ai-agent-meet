ca7aa2033098479bd3c9aa04f0bb3132656f7995
ALOK0078 <alokkumar0709003@gmail.com>
2025-06-04 04:04:49 +0530

    23 half done with completed page

--- src/modules/meetings/ui/components/columns.tsx
+++ src/modules/meetings/ui/components/columns.tsx
@@ -14,17 +14,11 @@ import { CornerDownRightIcon,
 import { Badge } from "@/components/ui/badge"
 import{format} from "date-fns"
 import humanizeDuration from "humanize-duration"
-import {cn} from "@/lib/utils";
+import {cn, formatDuration} from "@/lib/utils";
 
 
 
-function formatDuration(seconds:number){
-    return humanizeDuration(seconds*1000,{
-        largest:1,
-        round:true,
-        units:["h","m","s"],
-    })
-};
+
 const statusIconMap = {
     upcoming: ClockArrowUpIcon,
     active:LoaderIcon,
